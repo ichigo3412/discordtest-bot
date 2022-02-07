33d4fc38c28972497a1efa5eef0d57891cd9364b
Amir Muaz Mahran <joyous@Amirs-MacBook-Pro.local>
2022-02-08 01:13:21 +0800

    part 3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js")
 require("dotenv").config()
 
 const generateImage = require("./generateImage")
+const events = require("./handlers/events")
 
 const client = new Discord.Client({
     intents:[
@@ -11,7 +12,24 @@ const client = new Discord.Client({
     ]
 })
 
-client.on("ready", () => {
+let bot = {
+    client,
+    prefix: "n.",
+    owners: "243172504913313793"
+}
+
+client.command = new Discord.Collection()
+client.events = new Discord.Collection()
+
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands") (bot, reload)
+
+client.loadEvents(bot, false)
+client.loadCommands (bot, false)
+
+module.exports = bot
+
+/*client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
@@ -28,7 +46,7 @@ client.on("guildMemberAdd", async(member) =>{
     member.guild.channels.cache.get(WelcomeChannelId).send({
         content: `<@${member.id}> Welcome to the server`,
         files: [img]
-    })
-})
+    }
+})*/
 
 client.login(process.env.TOKEN)
